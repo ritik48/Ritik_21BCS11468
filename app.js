c9104a15048b24d9934ad3af619fdea1fe35086b
Ritik Raj <raj769417@gmail.com>
2023-09-23 14:00:51 +0530

    listen key strokes

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
 
-next.addEventListener("click", function () {
+function handleNext() {
     let items = document.querySelectorAll(".img-cont");
-    document
-        .querySelector(".container-slider")
-        .appendChild(items[0]);
-});
+    document.querySelector(".container-slider").appendChild(items[0]);
+}
 
-prev.addEventListener("click", function () {
+function handlePrevious() {
     let items = document.querySelectorAll(".img-cont");
     document
         .querySelector(".container-slider")
         .prepend(items[items.length - 1]); // here the length of items = 6
+}
+
+next.addEventListener("click", handleNext);
+prev.addEventListener("click", handlePrevious);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        handleNext();
+    } else if (event.key === "ArrowLeft") {
+        handlePrevious();
+    }
 });
